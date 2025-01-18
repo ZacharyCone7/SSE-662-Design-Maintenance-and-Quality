646f8976cbc0cb152ff06b525d5a1b160ee73bb2
ZacharyCone7 <zscone00@gmail.com>
2025-01-17 22:20:00 -0500

    Here

--- Module 1 Programming Task/main.go
+++ Module 1 Programming Task/main.go
@@ -14,13 +14,6 @@ import (
 		Status string // Pending/Done
 	}
 
-	// Define colors
-	const (
-		Reset = "\033[0m"
-		Red   = "\033[31m"
-		Green = "\033[32m"
-	)
-
 func main() {
 	var (
 		choice      int    // Store user input
